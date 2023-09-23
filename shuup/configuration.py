9b5cef000dbdba72d32c227a86e2537af7f6d3ed
Fupin Yao <xcmax@qq.com>
2023-09-23 14:41:10 -0700

    change styles

--- shuup/configuration.py
+++ shuup/configuration.py
@@ -6,7 +6,7 @@
 # This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
 """
-API for Shuup's Dynamic Configuration.
+API for Shuup's Dynamic Configuration. 
 
 Idea of the Dynamic Configuration is to allow storing configuration
 values similarly as :ref:`Django settings <django-settings-module>`
